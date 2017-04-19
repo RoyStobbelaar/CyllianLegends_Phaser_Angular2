ceca35c836d1b16592186fd7f10efa7708812ff6
Roy Stobbelaar <Roy Stobbelaar>
2017-04-19 22:59:09 +0200

    Debugmode is on again

--- src/app/components/game-component/game-classes/tileMap.ts
+++ src/app/components/game-component/game-classes/tileMap.ts
@@ -6,61 +6,52 @@ import { Vector2 } from './vector2';
 import { GameConfig } from './../game.config';
 
 //Just a collection of tiles
-export class TileMap{
+export class TileMap {
 
     public Collision: boolean;//This is a collision layer/map 
-    //public Tiles : Tile[];
-    public Tiles:number[][];
-    public Identifier : string;
+    public Tiles: number[][];
+    public Identifier: string;
     public spritesheet = new Image();
 
-    constructor(id:string, spritesheet:string, tiles:number[][], collision?:boolean){
-        //this.Tiles=tiles;
+    constructor(id: string, spritesheet: string, tiles: number[][], collision?: boolean) {
         this.spritesheet.src = GameConfig.game_image_path + '/' + spritesheet;
         this.Identifier = id;
-        this.Collision=collision;
+        this.Collision = collision;
         this.Tiles = tiles;
     }
 
     //Draw all tiles
-    public Draw(ctx:CanvasRenderingContext2D){
-        //this.Tiles.forEach(tile => tile.draw(ctx,this.Collision));
+    public Draw(ctx: CanvasRenderingContext2D) {
 
-        // if (GameConfig.debugMode) {
-        //     //Draw camera borders
-        //     if(this.Collision){
+        for (let i = 0; i < this.Tiles.length; i++) {
+            for (let j = 0; j < this.Tiles[i].length; j++) {
 
-        //         ctx.font = "16px Arial";
-        //         ctx.fillStyle = "#ffffff";
-        //         ctx.fillText(this.position.toString(), this.position.x - GameConfig.worldCamera.x, this.position.y - GameConfig.worldCamera.y);
+                var tileX = Math.floor(this.Tiles[i][j] / 16);
+                var tileY = this.Tiles[i][j] % 12;
 
-        //         ctx.rect(this.position.x - GameConfig.worldCamera.x,this.position.y - GameConfig.worldCamera.y,48,48);
-        //         ctx.strokeStyle = "#ff00ff";
-        //         ctx.stroke();
-        //     }
-        // }
+                ctx.drawImage(
+                    this.spritesheet,
+                    tileX * 48, tileY * 48, 48, 48,
+                    (i * 48) - GameConfig.worldCamera.x,
+                    (j * 48) - GameConfig.worldCamera.y,
+                    48, 48
+                );
 
-            for(let i = 0; i < this.Tiles.length; i++){
-                for(let j = 0; j < this.Tiles[i].length;j++){
+                if (GameConfig.debugMode && this.Collision) {
+                    ctx.font = "16px Arial";
+                    ctx.fillStyle = "#ffffff";
+                    ctx.fillText((i * 48).toString() + " - " + (j * 48).toString(), (i * 48) - GameConfig.worldCamera.x, (j * 48) - GameConfig.worldCamera.y);
 
-                    //Get y tile
-                    var tileX = Math.floor(this.Tiles[i][j]/16);
-                    //get x get rest
-                    var tileY = this.Tiles[i][j]%12;
-
-                    ctx.drawImage(
-                        this.spritesheet,
-                        tileX * 48, tileY * 48, 48, 48,
-                        (i * 48) - GameConfig.worldCamera.x,
-                        (j * 48) - GameConfig.worldCamera.y,
-                        48,48 
-                    );
+                    ctx.rect((i * 48) - GameConfig.worldCamera.x, (j * 48) - GameConfig.worldCamera.y, 48, 48);
+                    ctx.strokeStyle = "#ff00ff";
+                    ctx.stroke();
                 }
             }
+        }
     }
 
     //Check collision with playerlocation
-    public checkCollision(player: GameCharacter){
+    public checkCollision(player: GameCharacter) {
 
         // this.Tiles.forEach( (tile) => {
         //     if(Rectangle.Intersect(tile.BoundingBox,player.BoundingBox)){
